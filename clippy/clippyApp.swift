bff79e6811c92a343a1a00a63e4af31742609788
ujjwaltiwari <ujjwal.tiwari070@gmail.com>
2026-09-02 10:35:12 +0530

    Initial Commit

---
+++ clippy/clippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  clippyApp.swift
+//  clippy
+//
+//  Created by Ujjwal Tiwari on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct clippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
